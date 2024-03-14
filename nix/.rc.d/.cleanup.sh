3d558f547329040db3a914916be8a2e81ed5902e
Rahul Iyer <rahuliyer573@gmail.com>
2024-03-14 12:34:15 -0700

    Update cleanup script and gitconfig

--- nix/.rc.d/.cleanup.sh
+++ nix/.rc.d/.cleanup.sh
@@ -10,56 +10,72 @@ while true; do
   kill -0 "$$" || exit
 done 2> /dev/null &
 
+echo ""
+echo "╭─────────────╮"
+echo "│ 🗑️ Cleanup  │"
+echo "╰─────────────╯"
+echo ""
+
 old_available="$(df -k / | tail -1 | awk '{print $4}')"
 
-echo "Empty the Trash on all mounted volumes and the main HDD..."
+echo -en "ℹ️ Empty the Trash on all mounted volumes and the main HDD"
 sudo rm -rfv "/Volumes/*/.Trashes/*" &> /dev/null
 sudo rm -rfv "$HOME/.Trash/*" &> /dev/null
+echo -en "\r✅ Empty the Trash on all mounted volumes and the main HDD\n"
 
-echo "Clear System Log Files..."
+echo -en "ℹ️ Clear System Log Files"
 sudo rm -rfv "/private/var/log/asl/*.asl" &> /dev/null
 sudo rm -rfv "/Library/Logs/DiagnosticReports/*" &> /dev/null
 sudo rm -rfv "/Library/Logs/Adobe/*" &> /dev/null
 rm -rfv "$HOME/Library/Containers/com.apple.mail/Data/Library/Logs/Mail/*" &> /dev/null
 rm -rfv "$HOME/Library/Logs/*" &> /dev/null
+echo -en "\r✅ Clear System Log Files\n"
 
-echo "Clear Adobe Cache Files..."
+echo -en "ℹ️ Clear Adobe Cache Files"
 sudo rm -rfv "$HOME/Library/Application Support/Adobe/Common/Media Cache Files/*" &> /dev/null
+echo -en "\r✅ Clear Adobe Cache Files\n"
 
-echo "Cleanup iOS Applications..."
+echo -en "ℹ️ Cleanup iOS Applications"
 rm -rfv "$HOME/Music/iTunes/iTunes Media/Mobile Applications/*" &> /dev/null
+echo -en "\r✅ Cleanup iOS Applications\n"
 
-echo "Remove iOS Device Backups..."
+echo -en "ℹ️ Remove iOS Device Backups"
 rm -rfv "$HOME/Library/Application Support/MobileSync/Backup/*" &> /dev/null
+echo -en "\r✅ Remove iOS Device Backups\n"
 
-echo "Cleanup XCode Derived Data and Archives..."
+echo -en "ℹ️ Cleanup XCode Derived Data and Archives"
 rm -rfv "$HOME/Library/Developer/Xcode/DerivedData/*" &> /dev/null
 rm -rfv "$HOME/Library/Developer/Xcode/Archives/*" &> /dev/null
+echo -en "\r✅ Cleanup XCode Derived Data and Archives\n"
 
 if command -v brew > /dev/null; then
-  echo "Cleanup Homebrew Cache..."
+  echo -en "ℹ️ Cleanup Homebrew Cache"
   brew cleanup &> /dev/null
   brew cleanup --prune -s &> /dev/null
   rm -rfv "$(brew --cache)" &> /dev/null
   brew tap --repair &> /dev/null
+  echo -en "\r✅ Cleanup Homebrew Cache\n"
 fi
 
 if command -v docker > /dev/null; then
-  echo "Cleanup Docker..."
-  docker container prune -f
-  docker image prune -f
-  docker volume prune -f
-  docker network prune -f
+  echo -en "ℹ️ Cleanup Docker"
+  docker container prune -f &> /dev/null
+  docker image prune -f &> /dev/null
+  docker volume prune -f &> /dev/null
+  docker network prune -f &> /dev/null
+  echo -en "\r✅ Cleanup Docker\n"
 fi
 
 if command -v gem > /dev/null; then
-  echo "Cleanup gems..."
-  gem cleanup
+  echo -en "ℹ️ Cleanup Ruby Gems"
+  gem cleanup &> /dev/null
+  echo -en "\r✅ Cleanup Ruby Gems\n"
 fi
 
 for dir in "$HOME/.cache/pip" "$HOME/Library/Caches/pip" "$HOME/Library/Caches/pipenv"; do
-  echo "Cleaning pip cache from $dir..."
-  [ -d "$dir" ] && rm -rf "$dir"
+  echo -en "ℹ️ Cleaning pip cache from $dir"
+  [ -d "$dir" ] && rm -rf "$dir" && echo -en "\r✅" || printf "\r❌"
+  echo -en " Cleaning pip cache from $dir\n"
 done
 
 if [ -n "$PYENV_VIRTUALENV_CACHE_PATH" ]; then
@@ -68,41 +84,49 @@ if [ -n "$PYENV_VIRTUALENV_CACHE_PATH" ]; then
 fi
 
 if command -v npm > /dev/null; then
-  echo "Cleanup npm cache..."
-  npm cache clean --force
+  echo -en "ℹ️ Cleanup npm cache"
+  npm cache clean --force &> /dev/null
+  echo -en "\r✅ Cleanup npm cache\n"
 fi
 
 if command -v yarnpkg &> /dev/null; then
-  echo "Cleanup YarnPkg Cache..."
-  yarnpkg cache clean --force
+  echo -en "ℹ️ Cleanup YarnPkg cache"
+  yarnpkg cache clean --force &> /dev/null
+  echo -en "\r✅ Cleanup YarnPkg cache\n"
 fi
 
 declare -A CACHE_ALLOW_LIST=([antibody]=1 [com.spotify.client]=1)
 for dir in "$HOME/Library/Caches" "/Library/Caches"; do
-  echo "Cleanup files from $dir"
   if cd "$dir"; then
     while read -r file; do
       filename="$(basename "$file")"
+      fullpath="$(sudo realpath "$dir/$filename")"
       if [ "${CACHE_ALLOW_LIST[$filename]}0" != "0" ]; then
-        echo "Skipping $(realpath "$dir"/"$file")..."
+        echo -en "\r❌ Deleting $fullpath\n"
       else
-        echo "Deleting $(realpath "$dir"/"$file")..."
-        sudo rm -rf "$file"
+        echo -en "ℹ️ $fullpath"
+        if sudo rm -rf "$file" &>/dev/null; then
+          echo -en "\r✅"
+        else
+          echo -en "\r❌"
+        fi
+        echo -en " Deleting $fullpath\n"
       fi
     done < <(ls)
     cd - &> /dev/null || continue
+  else
+    echo -en "\r❌ Cleanup files from $dir\n"
   fi
 done
 
-echo "Purge inactive memory..."
+echo -en "ℹ️ Purge inactive memory"
 sudo purge
-
-echo "Success!"
+echo -en "\r✅ Purge inactive memory\n"
 
 new_available="$(df -k / | tail -1 | awk '{print $4}')"
 if type "numfmt" &> /dev/null; then
   count="$((new_available - old_available))"
   [ $count -lt 0 ] && count="0"
   count="${count}K"
-  echo "$(numfmt --from iec --to iec --suffix B $count) of space was cleaned up"
+  echo "🚀 $(numfmt --from iec --to iec --suffix B $count) of space was cleaned up"
 fi
