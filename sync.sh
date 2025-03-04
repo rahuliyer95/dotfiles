06732646b5893ec45e1b0e2fffee374e548394f8
Rahul Iyer <rahuliyer573@gmail.com>
2025-03-03 21:05:16 -0800

    Update `sync.sh` to create symlinks instead of copying files

--- sync.sh
+++ sync.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 DIRS=(nix common)
+
 case "$(uname -s)" in
-  Darwin*)
-    DIRS+=(macos)
-    ;;
+Darwin*)
+  DIRS+=(macos)
+  ;;
 esac
+
 for DIR in "${DIRS[@]}"; do
-  rsync -azvhrlP --exclude ".git" "./${DIR}/" "$HOME"
+  while read -r file; do
+    homepath="$(echo "$file" | sed "s/$DIR\///")"
+    rm "$HOME/$homepath"
+    ln -s "$PWD/$file" "$HOME/$homepath"
+  done < <(find "$DIR" -type f)
 done
