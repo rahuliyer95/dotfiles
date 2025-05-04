12eddd188e5b6a4d3eda9691772eeded824d79b5
Rahul Iyer <rahuliyer573@gmail.com>
2025-05-04 15:14:19 -0700

    ptpython: move configuration into a common location

---
+++ linux/.config/ptpython/config.py
@@ -0,0 +1,1 @@
+../../../packages/ptpython/config.py
--- sync.sh
+++ sync.sh
@@ -6,14 +6,18 @@ case "$(uname -s)" in
 Darwin*)
   DIRS+=(macos)
   ;;
+Linux*)
+  DIRS+=(linux)
+  ;;
 esac
 
 for DIR in "${DIRS[@]}"; do
   while read -r file; do
     homepath="$HOME/$(echo "$file" | sed "s/$DIR\///")"
     homedir="$(dirname "$homepath")"
+    echo "$file -> $homepath"
     rm "$homepath"
     mkdir -p "$homedir"
     ln -s "$PWD/$file" "$homepath"
-  done < <(find "$DIR" -type f)
+  done < <(find "$DIR" -type f,l)
 done
