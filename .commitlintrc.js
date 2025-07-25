8056b87039f085ecd7f115c064981c8ee7899b32
Rahul Iyer <rahuliyer573@gmail.com>
2025-07-25 11:05:19 -0700

    feat: add `pre-commit` config

---
+++ .commitlintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "chore", "revert"],
+    ],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+  },
+};
