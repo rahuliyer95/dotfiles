8f57360b2e0f9abd0f45995494d8f612005932fc
Rahul Iyer <rahuliyer573@gmail.com>
2025-11-12 11:32:57 -0800

    feat(nvim): update fold config

--- common/.config/nvim/lua/config/options.lua
+++ common/.config/nvim/lua/config/options.lua
@@ -37,6 +37,7 @@ vim.opt.undofile = true
 -- Fold
 vim.opt.foldenable = true
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevel = 9999
-vim.opt.foldcolumn = "1"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
