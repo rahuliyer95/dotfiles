3bf1567b4fa6f8ea942def8941f0c3dfe1e1ad88
Rahul Iyer <rahuliyer573@gmail.com>
2025-11-18 21:41:36 +0530

    fix(neovim): fix `fidget.nvim` config

--- common/.config/nvim/lua/plugins/fidget.lua
+++ common/.config/nvim/lua/plugins/fidget.lua
@@ -1,8 +1,15 @@
-require("fidget").setup({
+local fidget = require("fidget")
+
+fidget.setup({
   notification = {
     window = {
+      avoid = {
+        "NvimTree",
+      },
       border = "rounded",
       winblend = 0,
     },
   },
 })
+
+vim.notify = fidget.notify
